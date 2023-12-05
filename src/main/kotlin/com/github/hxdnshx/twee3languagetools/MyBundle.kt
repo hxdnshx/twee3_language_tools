fb992d4a7d158df3597e1e078f3e8443600c5fb5
GitHub Action <action@github.com>
2023-12-05 12:46:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/hxdnshx/twee3languagetools/MyBundle.kt
+++ src/main/kotlin/com/github/hxdnshx/twee3languagetools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hxdnshx.twee3languagetools
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hxdnshx/twee3languagetools/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/hxdnshx/twee3languagetools/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.hxdnshx.twee3languagetools.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hxdnshx.twee3languagetools.MyBundle
+import com.github.hxdnshx.twee3languagetools.services.MyProjectService
 import javax.swing.JButton
 
 
